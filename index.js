17858cc67f440b4242ec0b459cd4c836b9350dc8
slee713 <steven.lee1393@gmail.com>
2020-09-02 13:18:11 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let x = array.find(function(e) {return e.result === "W"})
+  if (x) {
+    return x.year
+  } 
+}
