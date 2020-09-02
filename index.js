a5514512d9c89f83882150f9e9af19c9caa8a26a
chsohn15 <chsohn15@gmail.com>
2020-09-02 14:12:59 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+function superbowlWin(record){
+
+  let found = record.find(function(game) {
+    return game.result === "W"
+  })
+  if (found !== undefined){
+    return found.year
+  }
+  else {
+    return undefined
+  }
+}
+
